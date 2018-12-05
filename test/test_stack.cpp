f599389615dba5e50fb7ae320eebf987ed62afc2
unknown <Serobjan.N@WS-6K-102-A13.ccam.cmc.unn.net>
2018-12-05 15:45:15 +0300

    add test 4

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,7 +1,35 @@
 #include "../tStack.h"
 #include "gtest.h"
 
-TEST(TStack,)
+TEST(TStack, can_push_element)
 {
-	//// add
+TStack<int> TS(3);
+
+ASSERT_NO_THROW(TS.Push(45));
+//system ("pause");
 }
+
+TEST(TStack, can_create_stack_with_positive_size)
+{
+ASSERT_NO_THROW(TStack<int> TS(6));
+}
+
+TEST(TStack, can_create_stack_with_negative_size)
+{
+ASSERT_ANY_THROW(TStack<int> TS(-6));  
+}
+
+TEST(TStack, can_create_copy_stack)
+{
+TStack<int> TS1(5);
+system ("pause");
+ASSERT_NO_THROW(TStack<int> TS2(TS1));
+}
+
+
+
+
+
+
+
+
